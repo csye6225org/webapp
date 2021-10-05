841c91a1f0e9598d737118df4b5c7f7214e59b58
Varad Lata Ratnakar Desai <varad.desai.rl@gmail.com>
2021-10-05 07:52:42 -0400

    Refined code using object oriented principles

--- project/src/main/java/com/edu/neu/csye6225/application/user/UserController.java
+++ project/src/main/java/com/edu/neu/csye6225/application/user/UserController.java
@@ -50,92 +50,69 @@ public class UserController {
         }
     }
 
+
     @GetMapping(path = "self")
-//    public ResponseEntity<Object> getUser(HttpServletRequest request){
     public ResponseEntity<Object> getUser(HttpServletRequest request){
-        String[] user_credentials; // Array of Strings to store user credentials.
 
-        String userHeader = request.getHeader("Authorization");
+        ResponseEntity<Object> header_authentication_result = userService.authenticateHeader(request);
 
-
-        if(userHeader.endsWith("Og==")) { // When No credentials are provided.
-            return new ResponseEntity<Object>("No credentials sent",HttpStatus.BAD_REQUEST);
-        }
-        else if (userHeader!=null && userHeader.startsWith("Basic")) { // When Header is correct
-            user_credentials = userService.getUserCredentials(userHeader);
-        }
-        else { // When authentication type is correct.
-            return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+        if(header_authentication_result.getStatusCode().equals(HttpStatus.BAD_REQUEST)){
+            return header_authentication_result;
+        } else {
+            String username = header_authentication_result.getBody().toString();
+            User user = userService.getUserByUsername(username);
+            return new ResponseEntity<Object>(userService.userResponseBody(user), HttpStatus.OK);
         }
 
-        User user;
-        if(!userService.checkIfUserExists(user_credentials[0])){ // When user does not exist in database.
-            return new ResponseEntity<Object>("User dont Exists",HttpStatus.BAD_REQUEST);
-        } else { // When correct user is getting requested.
-            user = userService.getUserByUsername(user_credentials[0]);
-            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-            if(!encoder.matches(user_credentials[1],user.getPassword())){ // When password is not correct.
-                return new ResponseEntity<Object>("Invalid Password",HttpStatus.BAD_REQUEST);
-            } else { // When everything is correct.
-                return new ResponseEntity<Object>(userService.userResponseBody(user), HttpStatus.OK);
-            }
-        }
     }
+
 
     @PutMapping(path = "self")
     public ResponseEntity<Object> updateUser(HttpServletRequest request, @RequestBody User user){
-        String[] user_credentials; // Array of Strings to store user credentials.
-        String userHeader = request.getHeader("Authorization");
 
-        if(userHeader.endsWith("Og==")) { // When No credentials are provided.
-            return new ResponseEntity<Object>("No credentials sent",HttpStatus.BAD_REQUEST);
-        }
-        else if (userHeader!=null && userHeader.startsWith("Basic")) { // When Header is correct
-            user_credentials = userService.getUserCredentials(userHeader);
-        }
-        else { // When authentication type is correct.
-            return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<Object> header_authentication_result = userService.authenticateHeader(request);
 
-        if(!userService.checkIfUserExists(user.getUsername())){ // When username in JSON of Request payload is changed
+        if(header_authentication_result.getStatusCode().equals(HttpStatus.BAD_REQUEST)){
+            return header_authentication_result;
+        } else if(!userService.checkIfUserExists(user.getUsername())){ // When username in JSON of Request payload is changed
             return new ResponseEntity<Object>(
                     "Username cannot be updated",
                     HttpStatus.BAD_REQUEST
             );
-        }
-
-        User user_from_database;
-        if(!userService.checkIfUserExists(user_credentials[0])){ // When user does not exist in database.
-            return new ResponseEntity<Object>("User dont Exists",HttpStatus.BAD_REQUEST);
-        } else { // When correct user existing in database is getting requested for update.
-            user_from_database = userService.getUserByUsername(user_credentials[0]);
-            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-            if(!encoder.matches(user_credentials[1],user_from_database.getPassword())){ // When password is not correct.
-                return new ResponseEntity<Object>("Invalid Password",HttpStatus.BAD_REQUEST);
-            } else { // When password is correct.
-
-                if (    user.getFirst_name() == null ||
+        } else if (    user.getFirst_name() == null ||
                         user.getLast_name() == null ||
                         user.getUsername() == null ||
                         user.getPassword() == null
-                ) { // When any of the field is null
-                    return new ResponseEntity<Object>(
-                            "Firstname, Lastname, Username and Password cannot be empty in JSON request body.",
-                            HttpStatus.BAD_REQUEST);
-                }
-                else { // When everything is correct.
-                    userService.updateUser(user);
+        ) { // When any of the field is null
+            return new ResponseEntity<Object>(
+                    "Firstname, Lastname, Username and Password cannot be empty in JSON request body.",
+                    HttpStatus.BAD_REQUEST);
+        }
+        else { // When everything is correct.
 
-                    return new ResponseEntity<Object>(
-                            userService.userResponseBody(userService.getUserByUsername(user_credentials[0])),
-                            HttpStatus.OK
-                    );
-                }
+            String username = header_authentication_result.getBody().toString();
+
+            if(!user.getUsername().equals(username)){
+                return new ResponseEntity<Object>(
+                        "Username in body dont match with username in credentials.",
+                        HttpStatus.BAD_REQUEST
+                        );
+            } else {
+                userService.updateUser(user);
+
+                return new ResponseEntity<Object>(
+                        userService.userResponseBody(userService.getUserByUsername(username)),
+                        HttpStatus.OK
+                );
             }
         }
 
+
+
     }
+
+
+
+
 
 }
--- project/src/main/java/com/edu/neu/csye6225/application/user/UserService.java
+++ project/src/main/java/com/edu/neu/csye6225/application/user/UserService.java
@@ -3,9 +3,11 @@ package com.edu.neu.csye6225.application.user;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.bcrypt.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.util.*;
 
@@ -66,32 +68,18 @@ public class UserService {
      * @return HttpStatus
      */
     public void updateUser(User user){
-//        System.out.println("userService.updateUser: A");
-//        if (    user.getFirst_name() == null ||
-//                user.getLast_name() == null ||
-//                user.getUsername() == null ||
-//                user.getPassword() == null
-//        ){
-//            System.out.println("userService.updateUser: B");
-//            return HttpStatus.BAD_REQUEST;
-//        } else if(!user.getUsername().equals(getUserByUsername(user.getUsername()).getUsername())){
-//            System.out.println("userService.updateUser: C");
-//            return HttpStatus.BAD_REQUEST;
-//        } else {
+
             User u = getUserByUsername(user.getUsername());
 
             System.out.println("1: "+u.toString());
             u.setFirst_name(user.getFirst_name());
             u.setLast_name(user.getLast_name());
-//            u.setPassword(user.getPassword());
             u.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
             u.setAccount_updated(LocalDateTime.now());
 
             System.out.println("2: "+u.toString());
             userRepository.save(u);
 
-//            return true;
-//        }
     }
 
 
@@ -138,6 +126,37 @@ public class UserService {
         userDetails.put("account_updated", user.getAccount_updated().toString());
 
         return userDetails;
+    }
+
+    public ResponseEntity<Object> authenticateHeader(HttpServletRequest request){
+
+        String[] user_credentials; // Array of Strings to store user credentials.
+        String userHeader = request.getHeader("Authorization");
+
+        if(userHeader.endsWith("Og==")) { // When No credentials are provided.
+            return new ResponseEntity<Object>("No credentials sent",HttpStatus.BAD_REQUEST);
+        }
+        else if (userHeader!=null && userHeader.startsWith("Basic")) { // When Header is correct
+            user_credentials = getUserCredentials(userHeader);
+        }
+        else { // When authentication type is correct.
+            return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+        }
+
+        User user_from_database;
+        if(!checkIfUserExists(user_credentials[0])){ // When user does not exist in database.
+            return new ResponseEntity<Object>("User dont Exists",HttpStatus.BAD_REQUEST);
+        } else { // When correct user existing in database is getting requested for update or get.
+            user_from_database = getUserByUsername(user_credentials[0]);
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+            if (    !encoder.matches(user_credentials[1],
+                    user_from_database.getPassword())) { // When password is not correct.
+                return new ResponseEntity<Object>("Invalid Password", HttpStatus.BAD_REQUEST);
+            } else { // When everything is correct.
+                return new ResponseEntity<Object>(user_from_database.getUsername(), HttpStatus.OK);
+            }
+        }
     }
 
 }
