0ce0ec5a8457a9f6c8cb2652debfb4f93679d4db
Varad Lata Ratnakar Desai <varad.desai.rl@gmail.com>
2021-10-31 16:03:00 -0400

    Changes to S3 client

--- project/src/main/java/com/edu/neu/csye6225/application/picture/AmazonS3Client.java
+++ project/src/main/java/com/edu/neu/csye6225/application/picture/AmazonS3Client.java
@@ -1,8 +1,5 @@
 package com.edu.neu.csye6225.application.picture;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,21 +9,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AmazonS3Client {
 
-    @Value("${amazonProperties.accessKey}")
-    private String accessKey;
-
-    @Value("${amazonProperties.secretKey}")
-    private String secretAccessKey;
-
     @Value("${amazonProperties.region}")
     private String awsRegion;
 
     @Bean
     AmazonS3 generateS3Client(){
-        AWSCredentials credentials=new BasicAWSCredentials(accessKey, secretAccessKey);
         return AmazonS3ClientBuilder
                 .standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(awsRegion)
                 .build();
     }
