acd19b8e6dca2e514fb8110641916749c31295d3
Varad Lata Ratnakar Desai <varad.desai.rl@gmail.com>
2021-11-03 22:53:19 -0400

    Commit for deployment

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -2,6 +2,8 @@ echo "***************************************************"
 echo "APPLICATION START BEGINS"
 echo "***************************************************"
 
+nohup java -jar /home/ubuntu/ROOT*.war server.port=8080 > /home/ubuntu/application-execution.out 2>&1 &
+
 echo "***************************************************"
 echo "APPLICATION START ENDS"
 echo "***************************************************"
