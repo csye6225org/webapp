8c65abf119f10b7caf35ddac72dd5f9cfec6f0f2
Varad Lata Ratnakar Desai <varad.desai.rl@gmail.com>
2021-11-25 18:37:02 -0500

    Fixing database read replica

--- project/src/main/java/com/edu/neu/csye6225/application/user/UserController.java
+++ project/src/main/java/com/edu/neu/csye6225/application/user/UserController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
 import org.apache.commons.validator.routines.EmailValidator;
 
@@ -190,7 +191,11 @@ public class UserController {
                         );
             } else {
                 logger.info("Controller updateUser: Updating user information.");
-                userService.updateUser(user);
+
+                User u = userService.getUserByUsername(user.getUsername());
+
+
+                userService.updateUser(u, user);
 
                 logger.info("Controller updateUser: User information updated successfully.");
                 logger.info("Controller updateUser: Returning updated user information.");
